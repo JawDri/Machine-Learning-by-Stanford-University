acb9dd97686d7398ce9f1a7ee3bc8c63576378c0
Jawher Dridi <dridijawher43@gmail.com>
2020-04-03 23:42:45 +0100

    Add files via upload

---
+++ Week4/lrCostFunction.m
@@ -0,0 +1,71 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+gx=X*theta;
+h=1+(e.^(-gx));
+hx=1./h;
+a=(y)'*(log(hx));
+b=(1-y)'*(log(1-hx));
+c=a+b;
+c=-c;
+thetasqr=0;
+n=size(theta);
+for i=2:n,
+  xpd=(theta(i)*theta(i));
+  thetasqr=thetasqr+xpd;
+  end;
+thetasqr=(lambda/(2*m)).*thetasqr;
+J=(c/m)+thetasqr;
+d=hx-y;
+f=X'*d;
+thetasum=(lambda/m).*theta;
+thetasum(1)=0;
+grad=((1/m).*f)+thetasum;
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
